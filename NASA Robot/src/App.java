7be464d1b049893d9837808e966e892fdcd4223c
PedroBarraza <pedropaschbarraza@gmail.com>
2022-06-23 21:50:48 -0300

    Feat: Added visualization.

--- NASA Robot/src/App.java
+++ NASA Robot/src/App.java
@@ -6,6 +6,29 @@ public class App {
         OESTE
     };    
 
+/* 
+    0       5     
+    1       4  
+    2 - 5 = -3 (*-1) = 3
+    3 - 5 = -2 (*-1) = 2
+    4 - 5 = -1 (*-1) = 1
+    5 - 5 = 0
+    */
+    //iremos fazer a função que mostra o robozin andando
+    public static void mostra(Rover robo, Plato plato){       
+        for(int i = 0; i <= plato.limiteX; i++){ //linhas
+            for(int j = 0; j <= plato.limiteY; j++){ //colunas
+                if(-1*(i - plato.limiteX) == robo.y && j == robo.x){
+                    System.out.print("R\t");
+                }
+                else{
+                    System.out.print(".\t");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("O robo esta em " + robo.x + " e em " + robo.y + ", olhando para o " + robo.direcao);
+    }
     public static void main(String[] args) throws Exception {
         InputReader input = new InputReader();
         Plato plato = input.retornaPlato();
@@ -15,6 +38,7 @@ public class App {
             char[] comandos = input.retornaComandos();
             for (int i = 0; i < comandos.length; i++){
                 rover.decifraComando(comandos[i], plato.limiteX, plato.limiteY);
+                mostra(rover, plato);
             }
             System.out.println("POSICAO FINAL: " + rover.x + " , " + rover.y + " , " + rover.direcao);
         }
